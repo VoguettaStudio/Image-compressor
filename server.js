fccb6769aa3879c2d314f9f5930a858b0f3437e3
Guido Marzocchini <guido.marzocchini@gmail.com>
2023-11-09 21:13:34 -0300

    corregi el formatting

--- server.js
+++ server.js
@@ -9,30 +9,27 @@ import convertVideo from './server_modules/converting.js';
 
 app.use(formidable());
 
-app.post('/formatting', (req, res) => {
-    
+app.post('/formatting', async (req, res) => {
     req.fields.size = parseInt(req.fields.size);
     
-    res.header('Content-Type', 'image/webp')
+    res.type('image/webp');
+    res.header('Content-Disposition', 'inline; filename=output.webp');
 
-    res.end(formatImage(req.fields, req.files));
+    res.send(await formatImage(req.fields, req.files));
 });
 
 app.post('/converting', async (req, res) => {
-    
     try {
         
         res.header('Content-Type', 'video/webm');
         res.header('Content-Disposition', 'inline; filename=output.webm');
 
         const convertedVideo = await convertVideo(req.files, res);
-    
         res.end(convertedVideo);
     
     } catch(error) {
         res.status(500).send(error.message);
     }
-    
 });
 
 app.listen(PORT, () => {
--- server_modules/formating.js
+++ server_modules/formating.js
@@ -4,11 +4,9 @@ import fs from 'fs'
 export default async function formatImage(fields, file){
 
     const imageBuffer = fs.readFileSync(file.image.path);
-
     var img = sharp(imageBuffer);
 
     const metadata = await img.metadata();
-
     const width = Math.round(metadata.width * fields.size / 100);
     const height = Math.round(metadata.height * fields.size /100);
 
@@ -16,5 +14,5 @@ export default async function formatImage(fields, file){
         .webp({quality: 70, effort:6})
         .resize({width: width, height: height});
 
-    return img;
+    return await img.toBuffer();
 }
