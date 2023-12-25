c61b174f0958372eebe41031c6ec4a3ad29e2979
Laza <lazacuestalautaro@gmail.com>
2023-12-25 19:43:55 -0300

    Add try to formatting

--- server.js
+++ server.js
@@ -1,37 +1,40 @@
-const PORT = 3000;
-import express from 'express';
+const PORT = 3002;
+import express from "express";
 const app = express();
 
-import formidable from 'express-formidable'
+import formidable from "express-formidable";
 
-import formatImage from './server_modules/formating.js'
-import convertVideo from './server_modules/converting.js';
+import formatImage from "./server_modules/formating.js";
+import convertVideo from "./server_modules/converting.js";
 
 app.use(formidable());
 
-app.post('/formatting', async (req, res) => {
-    req.fields.size = parseInt(req.fields.size);
-    
-    res.type('image/webp');
-    res.header('Content-Disposition', 'inline; filename=output.webp');
+app.post("/formatting", async (req, res) => {
+  req.fields.size = parseInt(req.fields.size);
 
+  res.type("image/webp");
+  res.header("Content-Disposition", "inline; filename=output.webp");
+
+  try {
     res.send(await formatImage(req.fields, req.files));
+  } catch (e) {
+    console.log(e);
+    res.status(500).send(e.message);
+  }
 });
 
-app.post('/converting', async (req, res) => {
-    try {
-        
-        res.header('Content-Type', 'video/webm');
-        res.header('Content-Disposition', 'inline; filename=output.webm');
+app.post("/converting", async (req, res) => {
+  try {
+    res.header("Content-Type", "video/webm");
+    res.header("Content-Disposition", "inline; filename=output.webm");
 
-        const convertedVideo = await convertVideo(req.files, res);
-        res.end(convertedVideo);
-    
-    } catch(error) {
-        res.status(500).send(error.message);
-    }
+    const convertedVideo = await convertVideo(req.files, res);
+    res.end(convertedVideo);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
 });
 
 app.listen(PORT, () => {
-    console.log(`http://localhost:${PORT}`)
+  console.log(`http://localhost:${PORT}`);
 });
